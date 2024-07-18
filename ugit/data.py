1cfa7ca6ffb435a203dd1146e1e3cdb668d3fe13
banutdragos <82029371+banutdragos@users.noreply.github.com>
2024-07-18 17:39:24 +0300

    Introduced the type tag for objects in order to lower the chance of using an object in the wrong context

--- ugit/data.py
+++ ugit/data.py
@@ -14,13 +14,22 @@ def init():
 
 
 # Reads and stores a file to .ugit\objects\ with its sha1 hash as the name
-def hash_object(data):
-    oid = hashlib.sha1(data).hexdigest()
+def hash_object(data, type_='blob'):
+    obj = type_.encode() + b'\x00' + data
+    oid = hashlib.sha1(obj).hexdigest()
     with open(f'{GIT_DIR}\\objects\\{oid}', 'wb') as out:
-        out.write(data)
+        out.write(obj)
     return oid
 
 
-def get_object(oid):
+def get_object(oid, expected='blob'):
     with open(f'{GIT_DIR}\\objects\\{oid}', 'rb') as f:
-        return f.read()
+        obj = f.read()
+
+    # Partitionam tipul si contentul folosindune de null byte
+    type_, _, content = obj.partition(b"\x00")
+    type_ = type_.decode()
+
+    if expected is not None and type_ !=expected:
+        raise ValueError(f'Exepected {expected}, got {type_}')
+
