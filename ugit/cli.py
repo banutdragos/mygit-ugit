9e2ee2ee294eeac5b0804eca7cbea5479ed08f87
banutdragos <82029371+banutdragos@users.noreply.github.com>
2024-07-18 14:00:27 +0300

    First commit. Init creates .ugit dir. Added ugit.exe to windows apps PATH as it's own path does not work for some reason. Aded arg parser to implemet sub-commands

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,2 +1,29 @@
-def main ():
-    print ('Hello, World!')
+# In charge of parsing and processing user input.
+
+
+import argparse
+import os  # modificarea/crearea fisierelor
+from . import data
+
+
+def main():
+    args = parse_args()
+    args.func(args)
+
+
+# Argument parser pentru a putea implementa subcomenzi gen commit, init
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
+def init(args):
+    data.init()
+    print(f"Initialized empty ugit repository in {os.getcwd()}/{data.GIT_DIR}")
